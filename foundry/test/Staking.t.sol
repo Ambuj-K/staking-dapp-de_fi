71004fde5aad13037fb07acf38f5fed07253eae3
Ambuj <ext.ambuj.kumar@stadiumgoods.com>
2022-10-19 17:21:31 +0530

    testing 3

--- foundry/test/Staking.t.sol
+++ foundry/test/Staking.t.sol
@@ -3,24 +3,48 @@ pragma solidity ^0.8.9;
 
 import "forge-std/Test.sol";
 import "../src/Staking.sol";
+import "../src/RewardToken.sol";
+
+interface CheatCodes {
+    // Skip forward block.timestamp
+    function warp(uint256) external;
+    // Set block.number
+    function roll(uint256) external;
+}
 
 contract StakingTest is DSTest {
+
+    CheatCodes constant cheats = CheatCodes(HEVM_ADDRESS);
+
     Staking public st;
+    RewardToken public token;
+    uint256 public constant staking_amt = 1 ether;
+
+    // setup/deploy staking & rewardtoken
     function setUp() public {
-        address addr = 0x1234567890123456789012345678901234567890;
-        st = new Staking(addr, addr);
+        token =new RewardToken();
+        st = new Staking(address(token), address(token));
     }
 
+    // testing staking function
     function testStake() public{
-        st.stake(.4 ether);
+        emit log_uint(staking_amt);
+        token.approve(address(st), staking_amt);
+        st.stake(staking_amt);
+        uint256 start_earned = st.earned(address(this));
+        emit log_uint(start_earned);
+        cheats.warp(1 days);
+        cheats.roll(block.number+1);
+        uint256 end_earned = st.earned(address(this));
+        emit log_uint(end_earned);
+        assertEq(end_earned,8639900);
     }
 
     function testWithDraw() public{
         st.withdraw(.2 ether);
     }
 
-    function testclaimReward() public{
-        
+    function testclaimReward() public{        
         st.claimReward();
     }
 
