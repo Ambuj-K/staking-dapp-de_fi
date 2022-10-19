65bfd4c491e6510544996a852c8abe6cef443a0b
Ambuj <ext.ambuj.kumar@stadiumgoods.com>
2022-10-19 10:09:38 +0530

    Frontend and testing

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import WalletConnect from "../lib/WalletConnect.svelte";
+    import contractAbi from "../contracts/Staking.json";
+    const contractAddr =  "0xCe743AD4F0b24c7238257e0E2f515448C87CC4b5";
+    export let web3Props = {
+        provider : null,
+        signer : null,
+        account : null,
+        chainId : null
+    };
+</script>
+
+<div class="OUTER">
+<h1>Staking App</h1>
+</div>
+{#if !web3Props.account}
+<WalletConnect bind:web3Props {contractAddr} {contractAbi} />
+{:else}
+Staking work in Progress.
+<br/>
+
+{/if}
